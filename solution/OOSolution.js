52a3eaa731ff40407c91ea28bc2d964e11097ac6
github username <github email address>
2017-09-15 11:40:41 -0400

    final

--- solution/OOSolution.js
+++ solution/OOSolution.js
@@ -1,1 +1,38 @@
 // Your OO/ES6/Prototype solution here!
+
+  let resultDiv = document.getElementById("result-display-container")
+  let userInputA = document.getElementById("opA")
+  let userInputB = document.getElementById('opB')
+  let operator = document.getElementById('operand')
+
+
+  let submitButton = document.getElementById("calculation-submitter")
+    // alert('on the page')
+    submitButton.addEventListener('click', ()=>{
+      console.log(userInputA.value)
+      let a = parseInt(userInputA.value)
+      console.log(userInputB.value)
+      let b = parseInt(userInputB.value)
+      console.log(operator.value)
+      let operand = operator.value
+
+function bothNumbers (a, b){ return (Number.isInteger(a) && Number.isInteger(b))}
+//if they ARE BOTH numbers then check for div by zero if not -dogeProtec
+
+
+if (bothNumbers(a, b) && !notDivByZero(b, operand)){
+  let answer =calculator(a, operand, b); resultDiv.innerHTML = answer} else {dogeProtec()}
+
+
+
+//after checking im not dividnig by zero - then excecute the calculation
+function modOrDivide (operand){return operand ===  "/" || operand === "%"}
+
+function notDivByZero (b, operand){
+  return modOrDivide(operand) && b === 0}
+
+
+    })
+
+//
+// })
--- solution/operations.js
+++ solution/operations.js
@@ -1,1 +1,30 @@
 // Your operation/calculation helper functions here!
+function calculator(a, operand, b){
+  // console.log (a, operand, b)
+  switch(operand) {
+    case "+":
+        return (a+b)
+        break;
+    case "-":
+        return (a-b)
+        break;
+    case "*":
+        return (a*b)
+        break;
+    case "/":
+        return (a/b)
+        break;
+    case "%":
+        return (a%b)
+        break;
+
+}
+/*
+
+
+*/
+  // (a operand b)
+  // alert('hi')
+  // alert(a operand b)
+  //return answer.. then put that in html  answerHTML
+}
